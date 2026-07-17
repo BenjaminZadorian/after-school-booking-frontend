697f4a06e6904ac46bd93557f6d2f89d880f73ce
Ben Zadorian <124893639+BenjaminZadorian@users.noreply.github.com>
2026-07-17 19:40:50 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
-  base: '/Full-Stack-Booking-Site-Frontend/',
+  base: '/after-school-booking-frontend/',
   build: {
     outDir: 'docs'
   },
